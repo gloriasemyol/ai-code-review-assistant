1e5ba8d40eb77f35c655d1c04234bd6362594255
Gloria Semyol <gloriasemyol@gmail.com>
2026-07-16 19:16:59 +0530

    deploy: enforce ignore build errors

--- frontend/src/app/components/LoadingSpinner.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center mt-20">
-      <div className="w-8 h-8 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-}
